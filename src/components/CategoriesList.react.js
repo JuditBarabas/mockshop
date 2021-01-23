dfbb17871ab10e88c15d858cef7fc3bf922cf0d9
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2021-01-23 20:13:00 +0100

    add basic material UI

--- src/components/CategoriesList.react.js
+++ src/components/CategoriesList.react.js
@@ -1,5 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
 
 function CategoriesList() {
   const products = useSelector((state) => state.products);
@@ -7,14 +22,26 @@ function CategoriesList() {
   const categoriesArr = Object.keys(products);
   const getFirstImage = (category) => products[category][0].image;
 
+  const classes = useStyles();
+
   return (
     <div>
       {categoriesArr.map((category, index) => {
         return (
-          <div key={index}>
-            <img src={getFirstImage(category)} alt="" width="100" />
-            <p>{category}</p>
-          </div>
+          <Card className={classes.root} key={index}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={getFirstImage(category)}
+                title="category img"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {category}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         );
       })}
     </div>
