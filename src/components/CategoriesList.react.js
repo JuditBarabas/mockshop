29484f9c26b55ef75957b2fbbc0dcf75b3bbfe43
JuditBarabas <judit.barabas1@gmail.com>
2021-01-29 15:51:54 +0100

    add Grid component to align Card components

--- src/components/CategoriesList.react.js
+++ src/components/CategoriesList.react.js
@@ -6,12 +6,11 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   root: {
-    width: 220,
-    float: "left",
-    margin: "3%"
+    marginTop: 10
   },
   media: {
     height: 160,
@@ -32,24 +31,34 @@ function CategoriesList() {
 
   return (
     <div>
-      {categoriesArr.map((category, index) => {
-        return (
-          <Card className={classes.root} key={index}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={getFirstImage(category)}
-                title="category img"
-              />
-              <CardContent>
-                <Typography className={classes.typography} gutterBottom variant="h5">
-                  {category}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        );
-      })}
+      <Grid
+        container
+        className={classes.root}
+        direction="row"
+        justify="center"
+        spacing={6}
+      >
+        {categoriesArr.map((category, index) => {
+          return (
+            <Grid item key={index}>
+              <Card className={classes.root} key={index}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={getFirstImage(category)}
+                    title="category img"
+                  />
+                  <CardContent>
+                    <Typography className={classes.typography} gutterBottom variant="h5">
+                      {category}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
