637a1fac6f37d9602814f84b2ec94bf51aa67ef3
JuditBarabas <judit.barabas1@gmail.com>
2021-02-20 20:39:28 +0100

    add displayed product details and some material ui

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,61 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    margin: 50,
+    boxShadow: "none"
+  },
+  containerMedia: {
+    width: 400
+  },
+  media: {
+    height: 300,
+    backgroundSize: "contain"
+  },
+  typography: {
+    marginBottom: 20 
+  },
+  discription: {
+    color: "#9e9e9e",
+    marginBottom: 20
+  }
+});
 
 function Product() {
   const productId = useSelector(state => state.productsInCategory.selectedProductID);
   const productsObj = useSelector(state => state.products.productsByID);
 
-  return <h1>{productsObj[productId].title}</h1>
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.container}>
+      <div className={classes.containerMedia}>
+      <CardMedia className={classes.media} image={productsObj[productId].image} />
+      </div>
+      <div >
+        <CardContent>
+        <Typography variant="h3" className={classes.typography}>
+          {productsObj[productId].title}
+        </Typography>
+        <Typography variant="h5" className={ classes.discription}>
+          {productsObj[productId].description}
+        </Typography>
+        <Typography variant="h5" className={classes.typography}>
+          Price: ${productsObj[productId].price}
+        </Typography>
+      </CardContent>
+      </div>
+      
+    </Card>
+  )
 }
 
 export default Product
