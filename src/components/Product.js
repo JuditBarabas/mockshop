6f6ef94f99f6066aac124b7decc4b0098030d907
JuditBarabas <judit.barabas1@gmail.com>
2021-02-21 15:00:16 +0100

    add size and qunatity selectors (to all products), no value saving

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from '@material-ui/core/Card';
@@ -6,6 +6,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 
 
 const useStyles = makeStyles({
@@ -28,37 +33,67 @@ const useStyles = makeStyles({
     color: "#9e9e9e",
     marginBottom: 20
   },
+  select: {
+    marginBottom: 20,
+    marginRight: 10,
+    width: 60
+  }
 });
 
 function Product() {
   const productId = useSelector(state => state.productsInCategory.selectedProductID);
   const productsObj = useSelector(state => state.products.productsByID);
 
+  const [ size, setSize ] = useState('');
+  const [ qunatity, setQuantity ] = useState(null);
+
   const classes = useStyles();
 
   return (
     <Card className={classes.container}>
+
       <div className={classes.containerMedia}>
-      <CardMedia className={classes.media} image={productsObj[productId].image} />
+        <CardMedia className={classes.media} image={productsObj[productId].image} />
       </div>
+
       <div >
         <CardContent>
-        <Typography variant="h3" className={classes.marginBottom}>
-          {productsObj[productId].title}
-        </Typography>
-        <Typography variant="h5" className={ classes.discription}>
-          {productsObj[productId].description}
-        </Typography>
-        <Typography variant="h5" className={classes.marginBottom}>
-          Price: ${productsObj[productId].price}
-        </Typography>
-        <Button variant="contained" color="primary" size="large" className={classes.button}>
-        ADD TO CART
-        </Button>
-      </CardContent>
-      
-      </div>
-      
+
+          <Typography variant="h3" className={classes.marginBottom}>
+            {productsObj[productId].title}
+          </Typography>
+
+          <Typography variant="h5" className={ classes.discription}>
+            {productsObj[productId].description}
+          </Typography>
+
+          <Typography variant="h5" className={classes.marginBottom}>
+            Price: ${productsObj[productId].price}
+          </Typography>
+
+          <FormControl className={classes.select}>
+            <InputLabel shrink>
+              Size
+            </InputLabel>
+            <Select value="S">
+              <MenuItem value="S">S</MenuItem>
+              <MenuItem value="M">M</MenuItem>
+              <MenuItem value="L">L</MenuItem>
+              <MenuItem value="XL">XL</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField type="number" label="Quantity" shrink defaultValue={1} className={classes.select}/>
+          
+          <div>
+            <Button variant="contained" color="primary" size="large" className={classes.button}>
+              ADD TO CART
+            </Button>
+          </div>
+        
+        </CardContent>
+      </div> 
+
     </Card>
   )
 }
